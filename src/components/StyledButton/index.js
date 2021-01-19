26c9ab9348237be56018995c80332f86cc73c633
Dejan Cvetkovski <dejancvetkovski@CVETKOVSKI-D-MAC.local>
2021-01-19 16:58:06 +0100

    chacnge test

--- src/components/StyledButton/index.js
+++ src/components/StyledButton/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import {View, Pressable, Alert, Text} from 'react-native';
-import styles from './style';
+import React from "react";
+import { View, Pressable, Alert, Text } from "react-native";
+import styles from "./style";
 
-const StyledButton = ({type, title, onPress}) => {
-  const background = type === 'primary' ? '#333' : 'white';
-  const textColor = type === 'primary' ? 'white' : '#333';
+const StyledButton = ({ type, title, onPress }) => {
+  const background = type === "primary" ? "#ddd" : "white";
+  const textColor = type === "primary" ? "white" : "#333";
 
   return (
     <View style={styles.container}>
       <Pressable
         onPress={() => onPress()}
-        style={[styles.button, {backgroundColor: background}]}>
-        <Text style={[styles.text, {color: textColor}]}>{title}</Text>
+        style={[styles.button, { backgroundColor: background }]}
+      >
+        <Text style={[styles.text, { color: textColor }]}>{title}</Text>
       </Pressable>
     </View>
   );
